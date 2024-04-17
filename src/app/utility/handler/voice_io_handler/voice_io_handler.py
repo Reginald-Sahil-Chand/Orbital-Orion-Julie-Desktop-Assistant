f14e50ecbca9ada466b4c686c6556b5b45655c70
Reginald Sahil Chand <Developer.ReginaldSahilChand@gmail.com>
2024-04-17 22:20:15 +1200

    Modularize voice_io_handler class. Inherit functionalities from it's parent abstract class

--- src/app/utility/handler/voice_io_handler/voice_io_handler.py
+++ src/app/utility/handler/voice_io_handler/voice_io_handler.py
@@ -7,7 +7,7 @@ This codebase will consist of comments based on humors at minimum to cheer up ot
 
 voice_io_handler.py:
 ====================
-This file contains a class to get voice inputs.
+This file contains a class to get voice inputs and perform tasks based on the given voice input.
 
 Guidelines:
 ===========
@@ -36,26 +36,19 @@ Refer to the module documentation for details.
 
 # Include built-in packages and modules.
 from dataclasses import dataclass, field
-from random import choice
-
-# Include internal types.
-from typing import Any
 
 # Include external packages and modules.
-from speech_recognition import ( # type: ignore
-    Recognizer,
-    Microphone,
-    UnknownValueError,
-    RequestError,
-)
+from speech_recognition import Recognizer, Microphone # type: ignore
 
 # Include custom packages and modules.
 from src.app.utility.handler.text_to_speech_handler.text_to_speech_handler\
     import TextToSpeechHandler
+from src.app.design_pattern.strategy.abstract.blueprint.abstract_voice_io_handler\
+    .abstract_voice_io_handler import AbstractVoiceIoHandler
 
 @dataclass
-class VoiceIoHandler:
-    """Class to get voice inputs."""
+class VoiceIoHandler(AbstractVoiceIoHandler):
+    """Class to get voice inputs, and perform tasks based on the given input."""
 
     # Instantiate TextToSpeechHandler.
     _text_to_speech_handler: TextToSpeechHandler = field(default_factory=TextToSpeechHandler)
@@ -64,42 +57,35 @@ class VoiceIoHandler:
     _recognizer: Recognizer = Recognizer()
     _microphone: Microphone = Microphone()
 
-    # Stores the voice input.
-    _voice_input: str = ""
+    # Stores the user's current speech at runtime.
+    _voice_input_value: (str | None) = ""
 
-    def get_voice_input(self) -> (str | None):
-        """Method that takes voice inputs and returns the input as a string.
+    def get_voice_input(self, speech_recognizer: str) -> None:
+        """Method that initiates the create_voice_recognizer method.
+
+        Summary:
+            - After the initiation of the create_voice_recognizer method the,
+            returned value (voice input) from that method is passed down to the required,
+            classes and functions that use it as needed.
+
+        Args:
+            - speech_recognizer (str): The speech recognizer name that will be used,
+            to distinguish between the supported speech recognizer's.
         
-        Returns (str): The voice_input, or None.
+        Returns: None
         """
 
         # Define announcement messages.
         noise_adjustment_message: str = (
             "A moment of silence please. Doing adjustments for ambient noise.")
+
         how_may_i_help_you_message: str = "How may I help you?\n"
         recognizing_voice_input_message: str = "Got it! Please wait. Recognizing voice input."
         voice_input_recognized_message: str = "You said"
+
         could_not_recognize_voice_input_message: str = (
             "Oops! I apologize because, I couldn't understand what you said. "
             "Could you please try again?\n")
-        request_error_message_for_google_speech_recognition: str = (
-            "Uh oh! Couldn't request results from Google Speech Recognition service.\n"
-            "Make sure you are connected to the internet. Good Bye.\n")
-
-        # Define wake words.
-        # * You can use these words to implement functionalities to only respond
-        # * If voice_input_value matches any of these words.
-        wake_words: list[str] = [
-            "hi", "hey", "hello", "Julie",
-            "hi Julie", "hello Julie", "hey Julie"
-        ]
-
-        greetings: list[str] = [
-            "Hi!", "Hello!", "Hey!", "Ola!",
-            "Bula",
-        ]
-
-        random_greeting: str = choice(greetings)
 
         try:
             print(noise_adjustment_message)
@@ -121,39 +107,17 @@ class VoiceIoHandler:
                 self._text_to_speech_handler.create_text_to_speech_announcer(
                     text_to_produce_speech=recognizing_voice_input_message)
 
-                # TODO: If you are using more than 1 speech recognition engine then,
-                # TODO: Please create each engine as a separate method or class, and invoke here.
-                # TODO: If you are to implement a class, consider creating it in a separate file.
-                # TODO: Each method or class must implement their own error handling using
-                # TODO: try catch.
-                # * NOTE: If you want you can import LogHandler() from utilities/handler
-                # * To log events.
+                self._voice_input_value = self.create_voice_recognizer(
+                    speech_recognizer=speech_recognizer,
+                    recognizer=self._recognizer,
+                    audio=audio,
+                    text_to_speech_handler=self._text_to_speech_handler,
+                    voice_input_recognized_message=voice_input_recognized_message,
+                    could_not_recognize_voice_input_message=could_not_recognize_voice_input_message)
 
-                try:
-                    # Recognize speech using Google Speech Recognition.
-                    voice_input_value: Any = self._recognizer.recognize_google(audio) # type: ignore
-                    self._voice_input = voice_input_value
-
-                    print(f"{voice_input_recognized_message, voice_input_value}\n")
-                    self._text_to_speech_handler.create_text_to_speech_announcer(
-                        text_to_produce_speech=(
-                            f"{voice_input_recognized_message, voice_input_value}"))
-
-                    if voice_input_value in wake_words:
-                        self._text_to_speech_handler.create_text_to_speech_announcer(
-                    text_to_produce_speech=random_greeting)
-
-                except UnknownValueError:
-                    print(could_not_recognize_voice_input_message)
-                    self._text_to_speech_handler.create_text_to_speech_announcer(
-                        text_to_produce_speech=could_not_recognize_voice_input_message)
-
-                except RequestError as err:
-                    print(f"{request_error_message_for_google_speech_recognition, err}")
-                    self._text_to_speech_handler.create_text_to_speech_announcer(
-                        text_to_produce_speech=(
-                            request_error_message_for_google_speech_recognition))
-                    return None
+                # Print returned value.
+                if isinstance(self._voice_input_value, str):
+                    print(self._voice_input_value.lower())
 
         except KeyboardInterrupt:
             print("Dear user, you have entered \"ctrl+c\", hence the program has been terminated.")
@@ -162,5 +126,3 @@ class VoiceIoHandler:
                     "Dear user, you have terminated the program by pressing the "
                     "control key with the letter C key available on your keyboard. "
                     "Good Bye and Take Care"))
-
-        return self._voice_input
