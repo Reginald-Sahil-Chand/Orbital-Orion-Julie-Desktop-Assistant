bf5a7faf833d48af0aa938db56b3601b1d0576e7
Reginald Sahil Chand <Developer.ReginaldSahilChand@gmail.com>
2024-04-19 19:17:41 +1200

    Add error handling

--- src/app/utility/helper/_module/app_opener/app_opener.py
+++ src/app/utility/helper/_module/app_opener/app_opener.py
@@ -33,12 +33,7 @@ Refer to the module documentation for details.
 from typing import Any, List
 
 # Include external packages and modules.
-from AppOpener import open as open_app # type: ignore
-
-# * NOTE: "# type: ignore" has been used at some places because, some packages,
-# * Does not have a specific stub file hence pylint / pylance are unable to infer types.
-# * Thus to avoid unnecessary errors and warnings, making the code base noisy, I decided to,
-# * Ignore types for some parts of the code.
+from AppOpener import open as open_app, give_appnames # type: ignore
 
 
 def open_application(query: str, text_to_speech_handler: Any) -> None:
@@ -55,20 +50,40 @@ def open_application(query: str, text_to_speech_handler: Any) -> None:
         - Exception: If an error occurs during the application opening process.
     """
 
+    app_names: Any = give_appnames(upper=False)
+
     _split_query_into_words: List[str] = query.split()[1::]
     _queried_application_name: str = " ".join(_split_query_into_words)
 
-    _app_not_found_error: str = (
-        f"{_queried_application_name} is not found in your device. Please try again!")
+    _app_not_found_error: str =(
+         "The requested application is not found in your device. Please try again!")
 
     try:
         # * open_app(_queried_application_name, match_closest=True, throw_error=True)
         # I actually dont use this because sometimes it gives weird results.
         # * Example: if the query = cloud -> Microsoft clock (if available in your device)
         # * will be opened.
-        open_app(_queried_application_name, match_closest=False, throw_error=True)
-        text_to_speech_handler.create_text_to_speech(
-                            text_to_produce_speech=f"{_queried_application_name} has been opened.")
+        open_app(_queried_application_name,
+                 match_closest=False,
+                 output=False,
+                 throw_error=True)
+
+        if _queried_application_name.strip():
+            if _queried_application_name in app_names:
+                text_to_speech_handler.create_text_to_speech(
+                                text_to_produce_speech=(
+                                    f"{_queried_application_name} has been opened."))
+
+            else:
+                _app_not_found_error = (
+                    f"{_queried_application_name} is not found in your device. Please try again!")
+
+                text_to_speech_handler.create_text_to_speech(
+                    text_to_produce_speech=_app_not_found_error)
+
+        else:
+            text_to_speech_handler.create_text_to_speech(
+                text_to_produce_speech=_app_not_found_error)
 
     except Exception:
         # We only speak the exception if "Speech Recognition" is not in the,
