fc04e92e34899e28b601aa52d22307a3e8d538d2
Reginald Sahil Chand <Developer.ReginaldSahilChand@gmail.com>
2024-04-21 00:33:20 +1200

    Remove unnecessary api key setup

--- src/app/home/_class/start/sr_ware_house/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Initialize Google's Gemini AI Api Key"""
-
-# Include external packages and modules.
-import google.generativeai as genai # type: ignore
-
-# Configure API KEY
-genai.configure(api_key="YOUR_API_KEY") # type: ignore
